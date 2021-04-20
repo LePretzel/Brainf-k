0e6a6fdfa63f1a58fa1702f2f3897dd8cae82a01
LePretzel <vince0782@gmail.com>
2021-04-19 22:15:08 -0500

    Make execute function for evaluating full program

--- src/Brainfuck.hs
+++ src/Brainfuck.hs
@@ -3,18 +3,23 @@ module Brainfuck
       startProgram,
       Command(..),
       evaluate,
+      execute,
     ) where
 
+import Control.Monad(when)
 import Data.Map(Map)
 import qualified Data.Map as Map
+import qualified Data.Text as T
 
-data Program = Program {instruction :: Int, loopIndices :: [Int], index :: Int, array :: Map Int Int } deriving (Show, Eq)
+type Arr = Map Int
+
+data Program = Program {instruction :: Int, loopIndices :: [Int], index :: Int, array :: Arr Int } deriving (Show, Eq)
 
 startProgram :: Program
 startProgram = Program {instruction = 0, loopIndices = [], index = 0, array = Map.fromList [(x, 0) | x <- [0..9]]}
 
 data Command = Increment | Decrement | ShiftLeft | ShiftRight |
-               Output | Input | OpenLoop | CloseLoop
+               Output | Input | OpenLoop | CloseLoop | None
 
 getIntInput :: IO Int 
 getIntInput = do 
@@ -28,7 +33,6 @@ evaluate Increment program = return program { array = Map.insertWith (+) (index 
 evaluate Decrement program = return program { array = Map.insertWith (+) (index program) (-1) (array program) }
 evaluate ShiftLeft program = return program { index = index program - 1 }
 evaluate ShiftRight program = return program { index = index program + 1 } 
-
 evaluate Output program = do 
     (putChar . toEnum) $ array program Map.! index program
     return program
@@ -41,4 +45,14 @@ evaluate OpenLoop program = return program { loopIndices = instruction program :
 
 evaluate CloseLoop program 
     | array program Map.! index program == 0    = return program {loopIndices = drop 1 $ loopIndices program }
-    | otherwise    = return program {instruction = head $ loopIndices program, loopIndices = drop 1 $ loopIndices program }       
+    | otherwise    = return program {instruction = head $ loopIndices program, loopIndices = drop 1 $ loopIndices program }      
+
+evaluate None program = return program
+
+
+execute :: Arr Command -> Program -> IO Program 
+execute com program = do
+    newProgram <- evaluate (com Map.! instruction program) program
+    if instruction program < Map.size com
+        then execute com newProgram
+        else return program
